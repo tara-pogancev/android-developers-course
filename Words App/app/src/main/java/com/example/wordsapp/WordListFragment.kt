b1e000be1f15b324c360359cf936d30fd3a0c3a0
Tara Pogančev <tarapogancev@gmail.com>
2022-10-27 09:46:07 +0200

    feat: words with fragments

---
+++ Words App/app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -0,0 +1,68 @@
+package com.example.wordsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.wordsapp.databinding.ActivityMainBinding
+import com.example.wordsapp.databinding.FragmentWordListBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [WordListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class WordListFragment : Fragment() {
+
+    private var _binding: FragmentWordListBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var letterId: String
+
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            letterId = it.getString(LETTER).toString()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWordListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //val letterId = activity?.intent?.extras?.getString(LETTER).toString()
+
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = WordAdapter(letterId, requireContext())
+
+        // Adds a [DividerItemDecoration] between items
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        )
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
