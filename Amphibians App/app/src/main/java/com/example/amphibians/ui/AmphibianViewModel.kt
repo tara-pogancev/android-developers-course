3d396bee6686eec414bb1daa140495dd3734b1c5
Tara Pogančev <tarapogancev@gmail.com>
2022-10-31 11:26:01 +0100

    feat: Amphibians project

--- Amphibians App/app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
+++ Amphibians App/app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
@@ -15,24 +15,48 @@
  */
 package com.example.amphibians.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.amphibians.network.Amphibian
+import com.example.amphibians.network.AmphibianApi
+import kotlinx.coroutines.launch
 
 enum class AmphibianApiStatus {LOADING, ERROR, DONE}
 
 class AmphibianViewModel : ViewModel() {
 
     // TODO: Create properties to represent MutableLiveData and LiveData for the API status
+    private val _status = MutableLiveData<AmphibianApiStatus>()
+    val status: LiveData<AmphibianApiStatus> = _status
 
     // TODO: Create properties to represent MutableLiveData and LiveData for a list of amphibian objects
+    private val _objects = MutableLiveData<List<Amphibian>>()
+    val objects: LiveData<List<Amphibian>> = _objects
 
     // TODO: Create properties to represent MutableLiveData and LiveData for a single amphibian object.
     //  This will be used to display the details of an amphibian when a list item is clicked
+    private val _amphibian = MutableLiveData<Amphibian>()
+    val amphibian: LiveData<Amphibian> = _amphibian
 
     // TODO: Create a function that gets a list of amphibians from the api service and sets the
     //  status via a Coroutine
+    fun getAmphibians() {
+        _status.value = AmphibianApiStatus.LOADING
+        try {
+            viewModelScope.launch {
+                _status.value = AmphibianApiStatus.DONE
+                _objects.value = AmphibianApi.retrofitService.getAmphibians()
+            }
+        } catch (e: Exception) {
+            _status.value = AmphibianApiStatus.ERROR
+            _objects.value = listOf()
+        }
+    }
 
     fun onAmphibianClicked(amphibian: Amphibian) {
         // TODO: Set the amphibian object
+        _amphibian.value = amphibian
     }
 }
